a1f379427f0257f5a3ce3865119ffec582d75da7
bogelegob <abdillahfarras74.com>
2024-07-09 15:39:52 +0700

    Oi udh selese ini

--- app/src/main/java/id/co/sigma/mewing/Model/UserModel.java
+++ app/src/main/java/id/co/sigma/mewing/Model/UserModel.java
@@ -1,16 +1,38 @@
 package id.co.sigma.mewing.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
-public class UserModel implements Serializable {
+public class UserModel implements Serializable, Parcelable {
 
     private String username;
-
     private String password;
-
     private String nama;
-
     private String jabatan;
+
+    public UserModel() {
+    }
+
+    protected UserModel(Parcel in) {
+        username = in.readString();
+        password = in.readString();
+        nama = in.readString();
+        jabatan = in.readString();
+    }
+
+    public static final Creator<UserModel> CREATOR = new Creator<UserModel>() {
+        @Override
+        public UserModel createFromParcel(Parcel in) {
+            return new UserModel(in);
+        }
+
+        @Override
+        public UserModel[] newArray(int size) {
+            return new UserModel[size];
+        }
+    };
 
     public String getUsername() {
         return username;
@@ -52,5 +74,18 @@ public class UserModel implements Serializable {
                 ", nama='" + nama + '\'' +
                 ", jabatan='" + jabatan + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(username);
+        dest.writeString(password);
+        dest.writeString(nama);
+        dest.writeString(jabatan);
     }
 }

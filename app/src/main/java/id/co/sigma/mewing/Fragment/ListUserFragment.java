24bcaf7ba36f1143b569745b9b0add35c21a926e
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-07-09 12:17:22 +0700

    item

--- app/src/main/java/id/co/sigma/mewing/Fragment/ListUserFragment.java
+++ app/src/main/java/id/co/sigma/mewing/Fragment/ListUserFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,11 +16,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import id.co.sigma.mewing.API.Repository.UserRepository;
+import id.co.sigma.mewing.API.VO.UserListVO;
+import id.co.sigma.mewing.Model.UserModel;
 import id.co.sigma.mewing.R;
 import id.co.sigma.mewing.ViewModel.UserViewModel;
 
 public class ListUserFragment extends Fragment {
+
+    private ListUserAdapter userAdapter;
+
+    private RecyclerView userRecyclerView;
 
     private UserViewModel mUserViewModel;
 
@@ -55,7 +67,53 @@ public class ListUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_user, container, false);
 
+        userRecyclerView = view.findViewById(R.id.list_data_user);
+        userRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        userAdapter = new ListUserAdapter();
+        userRecyclerView.setAdapter(userAdapter);
+
+        mUserViewModel.getAllUser();
+        mUserViewModel.getAllUserResponse().observe(getViewLifecycleOwner(), new Observer<UserListVO>() {
+            @Override
+            public void onChanged(UserListVO userListVO) {
+                if(userListVO != null){
+
+                }else {
+
+                }
+            }
+        });
+
         return view;
+    }
+
+    private class ListUserAdapter extends RecyclerView.Adapter<ListUserAdapter.ListUserHolder>{
+
+        private List<UserModel> userList = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public ListUserAdapter.ListUserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ListUserAdapter.ListUserHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class ListUserHolder extends RecyclerView.ViewHolder{
+
+            public ListUserHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 
     private void navigateToAddUser(){

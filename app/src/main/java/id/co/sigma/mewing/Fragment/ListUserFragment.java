61aa3e82c0a7b13e8b0b3504890abc9875233e1c
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-07-09 11:46:09 +0700

    Sigma

--- app/src/main/java/id/co/sigma/mewing/Fragment/ListUserFragment.java
+++ app/src/main/java/id/co/sigma/mewing/Fragment/ListUserFragment.java
@@ -2,65 +2,63 @@ package id.co.sigma.mewing.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import id.co.sigma.mewing.API.Repository.UserRepository;
 import id.co.sigma.mewing.R;
+import id.co.sigma.mewing.ViewModel.UserViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListUserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListUserFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private UserViewModel mUserViewModel;
 
     public ListUserFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListUserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListUserFragment newInstance(String param1, String param2) {
-        ListUserFragment fragment = new ListUserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_list_user, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.btn_new_user){
+            //navigateToAddUser();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        UserRepository.initialize(requireContext());
+        mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_user, container, false);
+
+        return view;
+    }
+
+    private void navigateToAddUser(){
+
     }
 }
